a5a0b487081375faf79f4caaceda2ea92ddf1168
20fut <231612@astanait.edu.kz>
2024-10-17 00:09:20 +0500

    concreate topping classes were created, added a extra costs

---
+++ src/assigment4/PepperoniTopping.java
@@ -0,0 +1,19 @@
+package assigment4;
+
+public class PepperoniTopping extends ToppingDecorator {
+
+    public PepperoniTopping(Pizza pizza) {
+        super(pizza);
+    }
+
+    @Override
+    public String getDescription() {
+        return pizza.getDescription() + ", Pepperoni";
+    }
+
+    @Override
+    public double getCost() {
+        return pizza.getCost() + 1.75;
+    }
+}
+
