e1f31b85f80d8bd37e8c98a7e870f30058f76d0b
20fut <231612@astanait.edu.kz>
2024-10-17 00:48:29 +0500

    character, adn its factory

---
+++ src/Outputs.java
@@ -0,0 +1,81 @@
+public class Outputs {
+}
+///1-Assigment)
+//Output:
+//Playing MP3 file. Name: song.mp3
+//Playing WAV file. Name: sound.wav
+//Playing AAC file. Name: music.aac
+//Unsupported format: flac
+//
+//2-Assigment)
+//Output:
+//TV is powered on.
+//TV channel set to 5
+//TV volume set to 15
+//TV is powered off.
+//
+//DVD Player is powered on.
+//DVD Player volume set to 20
+//DVD Player is powered off.
+//
+//Sound System is powered on.
+//Sound System volume set to 10
+//Sound System is powered off.
+//
+//3-Assigment)
+//Output:
+//Main Menu, All available items
+//---------------------
+//
+//Breakfast Menu, Morning dishes
+//---------------------
+//  Pancake, $5.99
+//     -- Delicious fluffy pancake
+//
+//Lunch Menu, Afternoon dishes
+//---------------------
+//  Burger, $8.99
+//     -- Classic beef burger with fries
+//  Pizza, $10.99
+//     -- Cheesy pepperoni pizza
+//
+//Drinks Menu, Beverages
+//---------------------
+//  Soda, $1.99
+//     -- Refreshing soda
+//
+//4-Assigment)
+//Output:
+//
+//Margherita Pizza, Cheese, Mushrooms Cost: $9.49
+//Vegetarian Pizza, Cheese, Cheese, Pepperoni Cost: $12.74
+//
+//5-Assigment)
+//Output:
+//Arriving home...
+//Lights are ON
+//Thermostat set to 22 degrees
+//Security system disarmed
+//
+//Activating movie mode...
+//Lights are OFF
+//Thermostat set to 20 degrees
+//Entertainment system set to Movie Mode
+//Entertainment system ON
+//
+//Activating night mode...
+//Lights are OFF
+//Thermostat set to 16 degrees
+//Security system armed
+//
+//Leaving home...
+//Lights are OFF
+//Thermostat set to 18 degrees
+//Security system armed
+//Entertainment system OFF
+//
+//6-Assigment)
+//Output:
+//
+//7-Assigment)
+//Output:
