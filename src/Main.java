99c495ce7deb9c6cea057571979fa6c3e2fd043d
20fut <231612@astanait.edu.kz>
2024-10-16 23:15:56 +0500

    main, cheking code

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,31 @@
-public class Main {
+import assigment2.*;
 
+public class Main {
+    public static void main(String[] args) {
+        Device tv = new TVDevice();
+        RemoteControl tvRemote = new BasicRemote(tv);
+
+        tvRemote.powerOn();
+        tvRemote.setChannel(5);
+        tvRemote.setVolume(15);
+        tvRemote.powerOff();
+
+        System.out.println();
+
+        Device dvd = new DVDDevice();
+        RemoteControl dvdRemote = new AdvancedRemote(dvd);
+
+        dvdRemote.powerOn();
+        dvdRemote.setVolume(20);
+        dvdRemote.powerOff();
+
+        System.out.println();
+
+        Device soundSystem = new SoundSystemDevice();
+        RemoteControl soundSystemRemote = new BasicRemote(soundSystem);
+
+        soundSystemRemote.powerOn();
+        soundSystemRemote.setVolume(10);
+        soundSystemRemote.powerOff();
+    }
 }
