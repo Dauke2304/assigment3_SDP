33cb812b04273740f8db62cdccf4339ee7929f05
20fut <231612@astanait.edu.kz>
2024-10-16 18:44:59 +0500

    Remote Control class

---
+++ src/assigment2/RemoteControl.java
@@ -0,0 +1,15 @@
+package assigment2;
+
+public abstract class RemoteControl {
+    protected Device device;
+
+    public RemoteControl(Device device) {
+        this.device = device;
+    }
+
+    public abstract void powerOn();
+    public abstract void powerOff();
+    public abstract void setChannel(int channel);
+    public abstract void setVolume(int volume);
+}
+
