09077aaf8a4ef95cc0a527b195403716ea7023e2
20fut <231612@astanait.edu.kz>
2024-10-16 18:22:56 +0500

    AdvancedAudioPlayer class

---
+++ src/assigment1/AdvancedAudioPlayer.java
@@ -0,0 +1,14 @@
+package assigment1;
+
+public class AdvancedAudioPlayer implements WAVPlayer, AACPlayer {
+    @Override
+    public void playWAV(String fileName) {
+        System.out.println("Playing WAV file - Name: " + fileName);
+    }
+
+    @Override
+    public void playAAC(String fileName) {
+        System.out.println("Playing AAC file - Name: " + fileName);
+    }
+}
+
