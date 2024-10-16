8ec2e17a4be544fe027c1a71ca271ad7009dd450
20fut <231612@astanait.edu.kz>
2024-10-17 00:38:57 +0500

    checking the code

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,22 @@
-import assigment4.*;
+import assigment5.*;
 public class Main {
     public static void main(String[] args) {
+        Light light = new Light();
+        Thermostat thermostat = new Thermostat();
+        SecuritySystem securitySystem = new SecuritySystem();
+        EntertainmentSystem entertainmentSystem = new EntertainmentSystem();
+
+        // Create the facade
+        SmartHomeFacade smartHome = new SmartHomeFacade(light, thermostat, securitySystem, entertainmentSystem);
+
+        // Simulate different actions
+        smartHome.arriveHome();
+        System.out.println();
+        smartHome.movieMode();
+        System.out.println();
+        smartHome.nightMode();
+        System.out.println();
+        smartHome.leaveHome();
 
 }
 }
