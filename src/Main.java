963b348f010505618461ad6745353f1763505bc0
20fut <231612@astanait.edu.kz>
2024-10-16 18:33:02 +0500

    Testing in main, works correctly(needed 3 tries)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import assigment1.MusicPlayerApp;
+
 public class Main {
     public static void main(String[] args) {
+        MusicPlayerApp playerApp = new MusicPlayerApp();
 
+        //MP3
+        playerApp.play("mp3", "song.mp3");
+
+        //WAV
+        playerApp.play("wav", "song2.wav");
+
+        // AAC
+        playerApp.play("aac", "song3.aac");
+
+        // Try playing unsupported format
+        playerApp.play("kim", "song4.kim");
     }
 }
