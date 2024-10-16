3d4841069e097c687a9c4967947ae86cbdf7b551
20fut <231612@astanait.edu.kz>
2024-10-16 18:46:01 +0500

    Basic remote class

---
+++ src/assigment2/BasicRemote.java
@@ -0,0 +1,28 @@
+package assigment2;
+
+public class BasicRemote extends RemoteControl {
+
+    public BasicRemote(Device device) {
+        super(device);
+    }
+
+    @Override
+    public void powerOn() {
+        device.powerOn();
+    }
+
+    @Override
+    public void powerOff() {
+        device.powerOff();
+    }
+
+    @Override
+    public void setChannel(int channel) {
+        device.setChannel(channel);
+    }
+
+    @Override
+    public void setVolume(int volume) {
+        device.setVolume(volume);
+    }
+}
