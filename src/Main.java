1701061f064ca7fe84f7b811cf97caa0706a1efd
20fut <231612@astanait.edu.kz>
2024-10-17 13:01:39 +0500

    testing the code

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
-import assigment7.*;
+import assigment6.*;
 public class Main {
     public static void main(String[] args) {
+        CharacterFactory characterFactory = new CharacterFactory();
+        TextEditor textEditor = new TextEditor(characterFactory);
+
+
+        textEditor.addCharacter('D', "Arial", 14, 10, 10);
+        textEditor.addCharacter('a', "Arial", 14, 20, 10);
+        textEditor.addCharacter('u', "Arial", 14, 30, 10);
+        textEditor.addCharacter('k', "Arial", 14, 40, 20);
+        textEditor.addCharacter('e', "Arial", 14, 50, 20);
+
+        textEditor.renderText();
 
 }
 }
